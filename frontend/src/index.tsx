1f4000a5c68a79c4ffe7c827ebf8c538b2a4fc09
ima_wizzard <aitor.vannevel@gmail.com>
2024-12-04 10:27:37 +0100

    added routes

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./views/App";
+import Login from "./views/Login";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
