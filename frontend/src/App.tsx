6120ceced0dd1d6ff25a063fc2828afbee7ea1d7
Robbe De Wispelaere <robbedewispelaere4@gmail.com>
2024-12-04 09:21:56 +0100

    tabel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,11 @@
 import "./App.css";
-import BarChartComponent from "./BarChartComponent";
-import TabelComponent from "./TabelComponent";
+import TabelComponent from "./components/TabelComponent";
 
 function App() {
   return (
     <div className="App">
       <h1>Bedrijfswinsten en Uitgaven per Jaar</h1>
-      <div className="chart-and-table">
-        <BarChartComponent />
-        <TabelComponent />
-      </div>
+      <TabelComponent />
     </div>
   );
 }
