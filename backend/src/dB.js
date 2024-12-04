a9e8d78a1ad897d1abff046e721ec4f8d530f65a
ima_wizzard <aitor.vannevel@gmail.com>
2024-12-04 08:37:48 +0100

    added backend + db

---
+++ backend/src/dB.js
@@ -0,0 +1,63 @@
+const mysql = require("mysql2");
+require("dotenv").config();
+
+let connection;
+
+try {
+  connection = mysql.createConnection({
+    password: process.env.MYSQL_PASSWORD,
+    user: process.env.MYSQL_USER,
+    host: "database",
+    port: 3306,
+    database: process.env.MYSQL_DATABASE,
+    insecureAuth: true,
+  });
+
+  try {
+    connection.connect((err) => {
+      console.log(
+        "trying to connect with this credentials",
+        process.env.MYSQL_PASSWORD,
+        process.env.MYSQL_USER,
+        process.env.MYSQL_DATABASE
+      );
+    });
+  } catch (err) {
+    process.exit(1);
+  }
+} catch (err) {
+  console.log(err);
+  process.exit(1);
+}
+
+connection.on("error", function (err) {
+  console.log("Error in database connection");
+  console.log(err);
+  process.exit(1);
+});
+
+connection.on("connect", function () {
+  console.log("connected to database");
+  createTable();
+});
+
+const createTable = () => {
+  const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS effects (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      preDefined BOOLEAN NOT NULL,
+      effectData TEXT NOT NULL
+    )
+  `;
+
+  connection.query(createTableQuery, (err) => {
+    if (err) {
+      console.error("Error creating table:", err);
+      return;
+    }
+    console.log("Table created successfully");
+  });
+};
+
+module.exports = { getEffects, saveEffect, loadEffect, deleteEffect };
