94ae037720ef33e2a42c27062c85f1916a3b3abd
Robbe De Wispelaere <robbedewispelaere4@gmail.com>
2024-12-04 08:43:12 +0100

    voorbeeld

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import BarChartComponent from "./BarChartComponent";
+import TabelComponent from "./TabelComponent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bedrijfswinsten en Uitgaven per Jaar</h1>
+      <div className="chart-and-table">
+        <BarChartComponent />
+        <TabelComponent />
+      </div>
     </div>
   );
 }
