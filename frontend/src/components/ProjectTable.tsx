4f080b97133df115d6fc82ee8db74810a41a6154
Robbe De Wispelaere <robbedewispelaere4@gmail.com>
2024-12-11 11:12:53 +0100

    dd

--- frontend/src/components/ProjectTable.tsx
+++ frontend/src/components/ProjectTable.tsx
@@ -99,7 +99,7 @@ function ProjectTable({ selectedProjectId }: { selectedProjectId: number }) {
       sortable: true,
       cell: (row: Row) => (
         <div title={row.Leveringsadres}>{row.Leveringsadres}</div>
-      ),
+      )
     },
     {
       name: "Datum aanvraag",
@@ -112,16 +112,16 @@ function ProjectTable({ selectedProjectId }: { selectedProjectId: number }) {
             month: "2-digit",
             year: "2-digit",
             hour: "2-digit",
-            minute: "2-digit",
+            minute: "2-digit"
           })}
         </div>
-      ),
+      )
     },
     {
       name: "Aantal",
       selector: (row: Row) => row.Aantal,
       sortable: true,
-      cell: (row: Row) => <div title={row.Aantal.toString()}>{row.Aantal}</div>,
+      cell: (row: Row) => <div title={row.Aantal.toString()}>{row.Aantal}</div>
     },
     {
       name: "Korte omschrijving",
@@ -129,13 +129,13 @@ function ProjectTable({ selectedProjectId }: { selectedProjectId: number }) {
       sortable: true,
       cell: (row: Row) => (
         <div title={row.Korte_omschrijving}>{row.Korte_omschrijving}</div>
-      ),
+      )
     },
     {
       name: "Winkel",
       selector: (row: Row) => row.Winkel,
       sortable: true,
-      cell: (row: Row) => <div title={row.Winkel}>{row.Winkel}</div>,
+      cell: (row: Row) => <div title={row.Winkel}>{row.Winkel}</div>
     },
     {
       name: "Artikelnummer",
@@ -143,7 +143,7 @@ function ProjectTable({ selectedProjectId }: { selectedProjectId: number }) {
       sortable: true,
       cell: (row: Row) => (
         <div title={row.Artikelnummer}>{row.Artikelnummer}</div>
-      ),
+      )
     },
     {
       name: "URL",
@@ -155,7 +155,7 @@ function ProjectTable({ selectedProjectId }: { selectedProjectId: number }) {
             Link
           </a>
         </div>
-      ),
+      )
     },
     {
       name: "Totale kostprijs excl. BTW",
@@ -165,7 +165,7 @@ function ProjectTable({ selectedProjectId }: { selectedProjectId: number }) {
         <div title={row.Totale_kostprijs_excl_BTW.toString()}>
           {row.Totale_kostprijs_excl_BTW}
         </div>
-      ),
+      )
     },
     {
       name: "Aangevraagd door",
@@ -173,7 +173,7 @@ function ProjectTable({ selectedProjectId }: { selectedProjectId: number }) {
       sortable: true,
       cell: (row: Row) => (
         <div title={row.Aangevraagd_door}>{row.Aangevraagd_door}</div>
-      ),
+      )
     },
     {
       name: "Aantal dagen levertijd",
@@ -183,7 +183,7 @@ function ProjectTable({ selectedProjectId }: { selectedProjectId: number }) {
         <div title={row.Aantal_dagen_levertijd.toString()}>
           {row.Aantal_dagen_levertijd}
         </div>
-      ),
+      )
     },
     {
       name: "Goedgekeurd door coach",
@@ -193,7 +193,7 @@ function ProjectTable({ selectedProjectId }: { selectedProjectId: number }) {
         <div title={row.Goedgekeurd_door_coach}>
           {row.Goedgekeurd_door_coach}
         </div>
-      ),
+      )
     },
     {
       name: "Bestelling ingegeven RQ nummer",
@@ -203,7 +203,7 @@ function ProjectTable({ selectedProjectId }: { selectedProjectId: number }) {
         <div title={row.Bestelling_ingegeven_RQ_nummer}>
           {row.Bestelling_ingegeven_RQ_nummer}
         </div>
-      ),
+      )
     },
     {
       name: "Bestelling door financieel dienst geplaatst",
@@ -218,11 +218,11 @@ function ProjectTable({ selectedProjectId }: { selectedProjectId: number }) {
               month: "2-digit",
               year: "2-digit",
               hour: "2-digit",
-              minute: "2-digit",
+              minute: "2-digit"
             }
           )}
         </div>
-      ),
+      )
     },
     {
       name: "Bestelling verzonden (verwachte aankomst)",
@@ -237,10 +237,10 @@ function ProjectTable({ selectedProjectId }: { selectedProjectId: number }) {
             month: "2-digit",
             year: "2-digit",
             hour: "2-digit",
-            minute: "2-digit",
+            minute: "2-digit"
           })}
         </div>
-      ),
+      )
     },
     {
       name: "Bestelling ontvangen datum",
@@ -253,17 +253,17 @@ function ProjectTable({ selectedProjectId }: { selectedProjectId: number }) {
             month: "2-digit",
             year: "2-digit",
             hour: "2-digit",
-            minute: "2-digit",
+            minute: "2-digit"
           })}
         </div>
-      ),
+      )
     },
     {
       name: "Opmerkingen",
       selector: (row: Row) => row.Opmerkingen,
       sortable: true,
-      cell: (row: Row) => <div title={row.Opmerkingen}>{row.Opmerkingen}</div>,
-    },
+      cell: (row: Row) => <div title={row.Opmerkingen}>{row.Opmerkingen}</div>
+    }
   ];
 
   const handleOrderAdd = () => {
@@ -286,7 +286,7 @@ function ProjectTable({ selectedProjectId }: { selectedProjectId: number }) {
         Bestelling_ingegeven_RQ_nummer: null, // Null voor optionele velden
         Bestelling_door_financ_dienst_geplaatst: false, // Boolean veld correct ingesteld
         Bestelling_verzonden_verwachtte_aankomst: null,
-        Bestelling_ontvangen_datum: null,
+        Bestelling_ontvangen_datum: null
       };
 
       await axios.post(`${backendUrl}/products`, newOrder);
